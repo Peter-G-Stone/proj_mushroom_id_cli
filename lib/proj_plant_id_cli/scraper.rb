7d9057dcca8b5d18dfbc58609395031629aeffd0
Peter-G-Stone <peter.stone.online@gmail.com>
2018-10-15 14:09:14 -0400

    cleaned up some comments, about to begin parsing the mushrooms individ prof pages

--- lib/proj_plant_id_cli/scraper.rb
+++ lib/proj_plant_id_cli/scraper.rb
@@ -24,7 +24,6 @@ class Scraper
 
     def self.scrape_index_page(index_url)
 
-
         doc = Nokogiri::HTML(open(index_url))
 
         
@@ -36,26 +35,39 @@ class Scraper
             plants_array << {name: doc.css("li")[i].text, link: "http://www.foragingguide.com" + "#{doc.css("li a")[i].attr("href")}"}
         end
         
-        binding.pry
-
-        # titleCounter = 0
-        # for i in 4...24
-        #     if i.even?
-        #         plants_array << {name: doc.css(".entry h2")[titleCounter].text, description: doc.css(".entry p")[i].text}
-        #         titleCounter += 1
-        #     end
-        # end
-
-        
-
-        # plants_array
-
-
+        plants_array
     end
 
-    #
+    
 
-#   def self.scrape_profile_page(profile_url)
+    def self.scrape_profile_page(profile_url)
+        doc = Nokogiri::HTML(open(profile_url))    
+        profile_hash = {
+            bio: doc.css(".description-holder p").text,
+            profile_quote: doc.css(".profile-quote").text
+        }       
+
+        social_array = doc.css(".social-icon-container a")
+        # self.parse_socials(profile_hash, social_array)
+    end
+
+#   def self.parse_socials(profile_hash, social_array)
+#     social_array.each do |noko_el|
+#       link = noko_el.attr("href")
+#       if link.include?("twitter")
+#         profile_hash[:twitter] = link
+#       elsif link.include?("facebook")
+#         profile_hash[:facebook] = link
+#       elsif link.include?("github")
+#         profile_hash[:github] = link
+#       elsif link.include?("linkedin")
+#         profile_hash[:linkedin] = link
+#       else
+#         profile_hash[:blog] = link
+#       end 
+#     end
+#     profile_hash
+#   end#   def self.scrape_profile_page(profile_url)
 #     doc = Nokogiri::HTML(open(profile_url))    
 #     profile_hash = {
 #       bio: doc.css(".description-holder p").text,
